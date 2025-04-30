1ed7272dbcc5ba61b3782be6f48fd1bd5ea8ce2b
realfishbread <yoonh12288@gmail.com>
2025-05-01 02:14:08 +0900

    3트

--- src/pages/board/api/CreatePost.js
+++ src/pages/board/api/CreatePost.js
@@ -2,12 +2,15 @@
 import axios from 'axios';
 
 export const CreatePost = async (formData) => {
+  const token = localStorage.getItem('accessToken'); // ✅ 여기 추가 필요!
+
   const response = await axios.post(
     'https://eventcafe.site/user/posts/create/',
     formData,
     {
       headers: {
         'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`, // ✅ 여기서 token 제대로 쓰기
       },
     }
   );
