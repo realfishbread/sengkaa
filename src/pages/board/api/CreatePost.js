9c6b355e0e374c402d25fb6dbc20f90c70ec5e77
realfishbread <yoonh12288@gmail.com>
2025-05-01 01:38:56 +0900

    fixed bug

--- src/pages/board/api/CreatePost.js
+++ src/pages/board/api/CreatePost.js
@@ -2,7 +2,11 @@
 import axiosInstance from '../../../shared/api/axiosInstance';
 
 export const CreatePost = async (formData) => {
-  const response = await axiosInstance.post('/user/posts/create/', formData);
+  const response = await axiosInstance.post('/user/posts/create/', formData, {
+    headers: {
+      // Content-Type 생략! axios가 자동 설정하게
+    },
+  });
   return response.data;
 };
 
