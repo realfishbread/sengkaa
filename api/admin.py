4bf92ed8d9ab1b8aec23edcc8002ce5e6b29de58
realfishbread <yoonh12288@gmail.com>
2025-05-23 18:40:46 +0900

    add admin file

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from api.models import Post
+from api.models import User, Report  # 커스텀 User 모델
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ['email', 'username', 'nickname', 'is_staff', 'is_superuser']
+    search_fields = ['email', 'username', 'nickname']
+    ordering = ['email']
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -11,3 +21,9 @@ class PostAdmin(admin.ModelAdmin):
     def approve_selected_posts(self, request, queryset):
         queryset.update(is_approved=True)
     approve_selected_posts.short_description = "✔️ 선택한 글 승인하기"
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['reporter', 'post', 'reason', 'created_at']
+    search_fields = ['reporter__nickname', 'post__title', 'reason']
+    list_filter = ['created_at']
