471ee5d690da72a3ae8eb119b532b195c8ac1cc2
realfishbread <yoonh12288@gmail.com>
2025-06-03 01:54:15 +0900

    add permission

--- api/views/main_views.py
+++ api/views/main_views.py
@@ -5,7 +5,8 @@ from api.serializers.main_serializer import MainBannerSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import permission_classes
 
-@permission_classes([AllowAny])
+
 class MainBannerListView(ListAPIView):
     queryset = MainBanner.objects.filter(is_active=True)
     serializer_class = MainBannerSerializer
+    permission_classes = [AllowAny]  # ✅ 이렇게
