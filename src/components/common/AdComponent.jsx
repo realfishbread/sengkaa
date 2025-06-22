affab6d211d9b4afbd9680767112f6cce79bbd29
realfishbread <yoonh12288@gmail.com>
2025-06-22 16:26:14 +0900

    fixed bug: common/AdComponent.jsx - csr 최적화

--- src/components/common/AdComponent.jsx
+++ src/components/common/AdComponent.jsx
@@ -1,5 +1,5 @@
-
 import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 const AdBanner = () => {
   useEffect(() => {
@@ -11,12 +11,21 @@ const AdBanner = () => {
   }, []);
 
   return (
+    <>
+      <Helmet>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-332"
+          crossOrigin="anonymous"
+        />
+      </Helmet>
     <ins className="adsbygoogle"
       style={{ display: 'block' }}
       data-ad-client="ca-pub-3324628579192842"  // 너의 퍼블리셔 ID
       data-ad-slot="4472276591"   // 광고 단위 ID
       data-ad-format="auto"
       data-full-width-responsive="true"></ins>
+    </>
   );
 };
 
